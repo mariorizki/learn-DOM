09fa74ed90923625e9b115c3127c33a6c4e5daf4
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-04 11:40:28 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -1,107 +1,115 @@
-let todos = [];
-let nextId = 0;
+let todos = []
+let nextId = 0
 
 const todoList = {
   displayTodo: () => {
-    let todosUl = document.querySelector('ul');
-    todosUl.innerHTML = '';
+    let todosUl = document.querySelector('ul')
+    todosUl.innerHTML = ''
 
     todos.forEach(todo => {
-      let todoLi = document.createElement('li');
-      todoLi.textContent = `ID: ${todo.id} - ${todo.todoText}`;
-      todosUl.appendChild(todoLi);
-    });
+      let todoLi = document.createElement('li')
+
+      if (todo.completed) {
+        todoLi.textContent = `ID: ${todo.id} - ${todo.todoText} [v]`
+      } else {
+        todoLi.textContent = `ID: ${todo.id} - ${todo.todoText} [x]`
+      }
+
+      todosUl.appendChild(todoLi)
+    })
   },
 
   addTodo: todoText => {
+    event.preventDefault()
+
     todos.push({
       id: nextId,
       todoText: todoText,
-      complete: false
-    });
+      completed: false
+    })
 
-    nextId++;
+    nextId++
   },
 
   deleteTodo: idNumber => {
     const deleted = todos.filter(todo => {
-      return todo.id !== idNumber;
-    });
+      return todo.id !== idNumber
+    })
 
-    todos = deleted;
+    todos = deleted
   },
 
   editTodo: (idNumber, todoText) => {
     todos.map(todo => {
       if (todo.id === idNumber) {
-        return (todo.todoText = todoText);
+        return (todo.todoText = todoText)
       } else {
-        return todos;
+        return todos
       }
-    });
+    })
   },
 
   toggleTodo: idNumber => {
     const toggleStatus = todos.map(todo => {
       if (todo.id === idNumber) {
-        return (todo.complete = !todo.complete);
+        return (todo.completed = !todo.completed)
       } else {
-        return todos;
+        return todos
       }
-    });
+    })
   },
 
   toggleAll: () => {
-    const totalTodo = todos.length;
-    let totalCompleteTodo = 0;
+    const totalTodo = todos.length
+    let totalCompleteTodo = 0
 
     todos.forEach(todo => {
-      if (todo.complete === true) {
-        totalCompleteTodo++;
+      if (todo.completed === true) {
+        totalCompleteTodo++
       }
-    });
+    })
 
     if (totalCompleteTodo === totalTodo) {
       todos.map(todo => {
-        return (todo.complete = false);
-      });
+        return (todo.completed = false)
+      })
     } else {
       todos.map(todo => {
-        return (todo.complete = true);
-      });
+        return (todo.completed = true)
+      })
     }
   }
-};
+}
 
 const handlers = {
   toggleAll: () => {
-    todoList.toggleAll();
-    todoList.displayTodo();
+    todoList.toggleAll()
+    todoList.displayTodo()
   },
 
   addTodo: () => {
-    let addTodoText = document.getElementById('addTodoText');
-    todoList.addTodo(addTodoText.value);
+    let addTodoText = document.getElementById('addTodoText')
+    todoList.addTodo(addTodoText.value)
 
-    addTodoText.value = '';
-    todoList.displayTodo();
+    addTodoText.value = ''
+    todoList.displayTodo()
   },
 
   editTodo: () => {
-    let inputTodoId = document.getElementById('inputTodoId');
-    let inputNewTodo = document.getElementById('inputNewTodo');
-    todoList.editTodo(inputTodoId.valueAsNumber, inputNewTodo.value);
+    let inputTodoId = document.getElementById('inputTodoId')
+    let inputNewTodo = document.getElementById('inputNewTodo')
+    todoList.editTodo(inputTodoId.valueAsNumber, inputNewTodo.value)
 
-    inputTodoId.value = '';
-    inputNewTodo = '';
-    todoList.displayTodo();
+    inputTodoId.value = ''
+    inputNewTodo = ''
+    todoList.displayTodo()
   },
 
   deleteTodo: () => {
-    let inputDeleteTodoId = document.getElementById('inputDeleteTodoId');
-    todoList.deleteTodo(inputDeleteTodoId.valueAsNumber);
+    let inputDeleteTodoId = document.getElementById('inputDeleteTodoId')
+    todoList.deleteTodo(inputDeleteTodoId.valueAsNumber)
 
-    inputDeleteTodoId = '';
-    todoList.displayTodo();
+    inputDeleteTodoId = ''
+    todoList.displayTodo()
   }
-};
+}
