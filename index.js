cdd7d9fd4df517b2e95323721c75479a4677f8e2
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-06 10:31:54 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-let nextId = 0;
-
 const Storage = {
   set: value => {
-    if (value !== null) localStorage.setItem('tasks', JSON.stringify(value));
+    if (value !== null) localStorage.setItem('tasks', JSON.stringify(value))
   },
 
   get: () => {
-    const data = JSON.parse(localStorage.getItem('tasks'));
-    if (data !== null) return data;
-    else return [];
+    const data = JSON.parse(localStorage.getItem('tasks'))
+    if (data !== null) return data
+    else return []
   }
-};
+}
+
+let nextId = 0
 
 const todoList = {
   data: Storage.get(),
 
   displayTodo: (data = todoList.data) => {
-    const todosDiv = document.getElementById('todos');
-    todosDiv.innerHTML = '';
+    const todosDiv = document.getElementById('todos')
+    todosDiv.innerHTML = ''
 
     todoList.data.forEach(todo => {
-      const todoLi = document.createElement('div');
-      todoLi.setAttribute('class', 'tasks');
+      const todoLi = document.createElement('div')
+      todoLi.setAttribute('class', 'tasks')
 
       if (todo.completed === true) {
         todoLi.innerHTML = `<span class="completed">${
@@ -30,7 +30,7 @@ const todoList = {
           todo.id
         })">X</button> <button onclick="todoList.editTodo(${
           todo.id
-        })" class="btn btn-warning">edit</button>`;
+        })" class="btn btn-warning">edit</button>`
       } else {
         todoLi.innerHTML = `${
           todo.todoText
@@ -38,94 +38,94 @@ const todoList = {
           todo.id
         })">X</button> <button onclick="todoList.editTodo(${
           todo.id
-        })" class="btn btn-warning">edit</button>`;
+        })" class="btn btn-warning">edit</button>`
       }
 
-      todosDiv.appendChild(todoLi);
-    });
+      todosDiv.appendChild(todoLi)
+    })
   },
 
-  addTodo: todoText => {
-    todoText.preventDefault();
+  addTodo: event => {
+    event.preventDefault()
 
     const newTodo = {
       id: nextId,
       todoText: document.getElementById('addTodoText').value,
       completed: false
-    };
+    }
 
     if (newTodo !== '') {
-      todoList.data.push(newTodo);
-      Storage.set(todoList.data);
-      document.getElementById('addTodoText').value = '';
-      nextId++;
-      todoList.displayTodo();
+      todoList.data.push(newTodo)
+      Storage.set(todoList.data)
+      document.getElementById('addTodoText').value = ''
+      nextId++
+      todoList.displayTodo()
     }
   },
 
   deleteTodo: idNumber => {
     const deleted = todoList.data.filter(todo => {
-      return todo.id !== idNumber;
-    });
+      return todo.id !== idNumber
+    })
 
-    todoList.data = deleted;
-    Storage.set(todoList.data);
-    todoList.displayTodo();
+    todoList.data = deleted
+    Storage.set(todoList.data)
+    todoList.displayTodo()
   },
 
   editTodo: idNumber => {
-    const textInput = prompt('Edit task to...');
+    const textInput = prompt('Edit task to...')
 
     if (textInput !== null) {
       const edited = todoList.data.map(todo => {
-        if (todo.id === id) {
-          todo.todoText = textInput;
+        if (todo.id === idNumber) {
+          todo.todoText = textInput
         }
-        return todo;
-      });
+        return todo
+      })
 
-      todoList.data = edited;
-      Storage.set(todoList.data);
-      todoList.displayTodo();
+      todoList.data = edited
+      Storage.set(todoList.data)
+      todoList.displayTodo()
     }
   },
 
   toggleTodo: idNumber => {
     const toggleStatus = todoList.data.map(todo => {
       if (todo.id === idNumber) {
-        return (todo.completed = !todo.completed);
+        return (todo.completed = !todo.completed)
       } else {
-        return todoList.data;
+        return todoList.data
       }
-    });
+    })
 
-    Storage.set(todoList.data);
-    todoList.displayTodo();
+    Storage.set(todoList.data)
+    todoList.displayTodo()
   },
 
   toggleAll: () => {
-    const totalTodo = todoList.data.length;
-    let totalCompleteTodo = 0;
+    const totalTodo = todoList.data.length
+    let totalCompleteTodo = 0
 
     todoList.data.forEach(todo => {
       if (todo.completed === true) {
-        totalCompleteTodo++;
+        totalCompleteTodo++
       }
-    });
+    })
 
     if (totalCompleteTodo === totalTodo) {
       todoList.data.map(todo => {
-        return (todo.completed = false);
-      });
+        return (todo.completed = false)
+      })
     } else {
       todoList.data.map(todo => {
-        return (todo.completed = true);
-      });
+        return (todo.completed = true)
+      })
     }
 
-    Storage.set(todoList.data);
-    todoList.data(displayTodo);
+    Storage.set(todoList.data)
+    todoList.data(displayTodo)
   }
-};
+}
 
-todoList.displayTodo();
+todoList.displayTodo()
